7038d6be83298967735df8eff752f84b29227bd2
ktkalpha <57173745+ktkalpha@users.noreply.github.com>
2022-12-15 21:36:44 +0900

    weather and background and css

--- js/background.js
+++ js/background.js
@@ -2,8 +2,6 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const bgImage = document.body;
 
-bgImage.src = `./img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+bgImage.style = `background-image: url("./img/${chosenImage}");`;
--- js/weather.js
+++ js/weather.js
@@ -3,16 +3,18 @@ const API_KEY = "db11cce9af952233874dc9bc8bcf6a13";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&&lang=kr`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
+      const weatherIcon = document.querySelector("#weather-icon");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${parseInt(
         data.main.temp
-      )}`;
+      )}°C,`;
+      weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     });
 }
 function onGeoError() {
